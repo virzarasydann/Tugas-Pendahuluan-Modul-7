590af7a4346105811df3f95627e1cd0fc83f7490
virzarasydann <virzarasydan@gmail.com>
2026-04-05 23:11:32 +0700

    Menambahkan class DataMahasiswa103082400034

---
+++ tpModul7_kelompok_1/DataMahasiswa103082400034.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace tpModul7_kelompok_1
+{
+    internal class DataMahasiswa103082400034
+    {
+        public string nama { get; set; }
+        public long nim { get; set; }
+        public string fakultas { get; set; } 
+
+        
+        public void ReadJSON()
+        {
+            string filePath = "tp7_1_103082400034.json";
+
+            try
+            {
+               
+                string jsonString = File.ReadAllText(filePath);
+
+               
+                DataMahasiswa103082400034 mahasiswa = JsonSerializer.Deserialize<DataMahasiswa103082400034>(jsonString);
+
+                
+                Console.WriteLine($"Nama {mahasiswa.nama} dengan nim {mahasiswa.nim} dari fakultas {mahasiswa.fakultas}");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File JSON tidak ditemukan. Pastikan file berada di folder yang sama dengan program.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Terjadi kesalahan: {ex.Message}");
+            }
+        }
+    }
+}
